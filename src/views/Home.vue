c121aec985eb2d1b367b81f83ca2284f8e1f2c3b
omekrit <khomkrit.y@gmail.com>
2021-07-14 23:37:09 +0700

    add loading tag

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="home">Hello World</div>
+  <main v-if="!loading">Show Data</main>
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-gray-500 text-3xl mt-10 mb-6">Fetching Data</div>
+    <img :src="loadingImage" class="w-24 m-auto" alt="loading" />
+  </main>
 </template>
 
 <script>
@@ -9,26 +13,26 @@ export default {
   data() {
     return {
       loading: true,
-      title: 'Global',
-      dataDate: '',
+      title: "Global",
+      dataDate: "",
       stats: {},
       countries: [],
-      loadingImage: require('../assets/motion.gif')
-    }
+      loadingImage: require("../assets/motion.gif"),
+    };
   },
   methods: {
-    async fetchCovidData(){
-      const res = await fetch('https://api.covid19.com/summary');
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19.com/summary");
       const data = await res.json();
       return data;
-    }
+    },
   },
-  async created(){
+  async created() {
     const data = await this.fetchCovidData();
     this.dataDate = data.Date;
     this.stats = data.Global;
     this.countries = data.Countries;
     this.loading = false;
-  }
+  },
 };
 </script>
