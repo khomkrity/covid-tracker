226725e4b90b18002525a56ed4448c697ced045a
omekrit <khomkrit.y@gmail.com>
2021-07-14 23:31:27 +0700

    fect covid-19 api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,29 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      loading: true,
+      title: 'Global',
+      dataDate: '',
+      stats: {},
+      countries: [],
+      loadingImage: require('../assets/motion.gif')
+    }
+  },
+  methods: {
+    async fetchCovidData(){
+      const res = await fetch('https://api.covid19.com/summary');
+      const data = await res.json();
+      return data;
+    }
+  },
+  async created(){
+    const data = await this.fetchCovidData();
+    this.dataDate = data.Date;
+    this.stats = data.Global;
+    this.countries = data.Countries;
+    this.loading = false;
+  }
 };
 </script>
